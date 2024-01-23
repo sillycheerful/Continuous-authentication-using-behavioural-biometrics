799d933f38547397f1c7a53aa7896aa2c94497c4
sillycheerful <sophieraccagna01@gmail.com>
2024-01-23 22:47:29 +0000

    create secondary window 3 times

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from tkinter import *
 
 class MyApp:
-    def __init__(self, root):
+    def __init__ (self, root):
         self.root = root
         self.root.title("My App")
-        self.root.geometry("400x400")
+        self.root.geometry("200x200")
 
 
         self.label = Label(self.root, text="This is my app!")
@@ -13,15 +13,20 @@ class MyApp:
         self.print_button = Button(self.root, text="Print Me", command=self.print_something)
         self.print_button.pack()
 
+    #doesnt currently work lol
         self.record_button = Button(self.root, text="Record", command=self.record_mouse_movement)
         self.record_button.pack()
 
         self.exit_button = Button(self.root, text="Exit", command=self.root.quit)
         self.exit_button.pack()
 
+        for i in range(3):
+            self.create_secondary_window()
+
+    def create_secondary_window(self):
         self.secondary_window= Toplevel(self.root)
         self.secondary_window.title("Speed is being recorded")
-        self.secondary_window.geometry("300x300")
+        self.secondary_window.geometry("100x100")
 
         self.secondary_window_lable= Label(self.secondary_window,text="this is my 2nd window")
         self.secondary_window_lable.pack()
@@ -30,14 +35,16 @@ class MyApp:
         self.secondary_window_button.pack()
 
     def print_something(self):
-        print("Hey whatsup bro, I am doing something very interesting.")
+        print("Printing something...")
 
     def record_mouse_movement(self):
         def motion(event):
             x, y = event.x, event.y
             print(f"Mouse position: ({x}, {y})")
-        self.root.bind("<Motion>", motion)
+            self.root.bind("<Motion>", motion)
 
 root = Tk()
 my_app = MyApp(root)
 root.mainloop()
+
+
