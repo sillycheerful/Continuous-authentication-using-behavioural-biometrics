b2e1cb9603dd2fb39b23ce844487d604ca250138
sillycheerful <sophieraccagna01@gmail.com>
2024-01-26 12:09:23 +0000

    Show pop-up asking user to click on OK Fixes #8

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from tkinter import *
+import time
 
 class MyApp:
     def __init__ (self, root):
+        self.create_primary_window()
+
+        for i in range(3):
+            time.sleep(5)
+            self.create_secondary_window()
+
+    def create_primary_window(self):
         self.root = root
         self.root.title("My App")
         self.root.geometry("200x200")
@@ -13,15 +21,23 @@ class MyApp:
         self.print_button = Button(self.root, text="Print Me", command=self.print_something)
         self.print_button.pack()
 
-    #doesnt currently work lol
+        #doesnt currently work lol
         self.record_button = Button(self.root, text="Record", command=self.record_mouse_movement)
         self.record_button.pack()
 
         self.exit_button = Button(self.root, text="Exit", command=self.root.quit)
         self.exit_button.pack()
 
-        for i in range(3):
-            self.create_secondary_window()
+    def create_secondary_window(self):
+        self.secondary_window= Toplevel(self.root)
+        self.secondary_window.title("Speed is being recorded")
+        self.secondary_window.geometry("100x100")
+
+        self.secondary_window_lable= Label(self.secondary_window,text="this is my 2nd window")
+        self.secondary_window_lable.pack()
+
+        self.secondary_window_button=Button(self.secondary_window, text="Ok", command=self.secondary_window.destroy)
+        self.secondary_window_button.pack()
 
     def create_secondary_window(self):
         self.secondary_window= Toplevel(self.root)
