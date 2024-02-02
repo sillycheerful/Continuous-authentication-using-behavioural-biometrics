2d8c1be093322ac061a891bfb3c4daf8e7ce7542
sillycheerful <sophieraccagna01@gmail.com>
2024-02-02 12:31:35 +0000

    Started work on showing window requiring text input

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from tkinter import *
+#https://www.geeksforgeeks.org/how-to-get-the-input-from-tkinter-text-box/
+import tkinter as Tk
 
 class MyApp:
     def __init__ (self, root):
         self.root = root
         self.root.title("My App")
         self.root.geometry("200x200")
-
 
         self.label = Label(self.root, text="This is my app!")
         self.label.pack()
@@ -19,6 +20,8 @@ class MyApp:
 
         self.exit_button = Button(self.root, text="Exit", command=self.root.quit)
         self.exit_button.pack()
+
+        self.text_window()
 
         for i in range(3):
             self.create_secondary_window()
@@ -34,6 +37,17 @@ class MyApp:
         self.secondary_window_button=Button(self.secondary_window, text="Ok", command=self.secondary_window.destroy)
         self.secondary_window_button.pack()
 
+    def text_window(self):
+        self.text_window= Toplevel(self.root)
+        self.text_window.title("Please type")
+        self.text_window.geometry("100x100")
+
+        # https://www.geeksforgeeks.org/how-to-get-the-input-from-tkinter-text-box/
+        frame= tk.Tk
+        input_box= Tk.text(frame, height=30, width=90)
+        input_box.pack()
+
+
     def print_something(self):
         print("Printing something...")
 
@@ -46,5 +60,4 @@ class MyApp:
 root = Tk()
 my_app = MyApp(root)
 root.mainloop()
-
 
